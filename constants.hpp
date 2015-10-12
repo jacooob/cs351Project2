d53204042fdbbab6929e7f8b9f56d0722affacd2
Jacob Allen <jahocob@gmail.com>
2015-10-11 19:27:09 -0700

    Update constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -10,14 +10,14 @@ const std::string reg_names[32] = {
     "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
     "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
     "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"};
-
+// opcode names by their deciaml value
 const std::string instruction_names[43] = {
   "\0", "\0", "J", "JAL", "BEQ", "BNE", "\0", "\0", "ADDI",
   "ADDIU", "SLTI", "SLTIU", "ANDI", "ORI", "\0", "LUI", "\0",
   "\0", "\0", "\0", "\0", "\0", "\0", "\0", "\0", "\0", "\0",
   "\0", "\0", "\0", "\0", "\0", "LB", "LH", "\0", "LW", "LBU",
   "LHU", "\0", "\0", "SB", "SH", "\0", "SW"};
-
+// functions by their decimal value
 const std::string rformat_instruction[43] = {
   "SLL", "\0", "SRL", "\0", "\0", "\0", "\0", "\0", "JR",
   "\0", "\0", "\0", "\0", "\0", "\0", "\0", "\0", "\0",
